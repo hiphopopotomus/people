502afe9d45618762f1b6c903a90e288958d85373
Espen Grønli <espen.gronli@outlook.com>
2019-11-20 09:59:24 +0100

    Add monotonic ID-generator

---
+++ web-api/Util/IdGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webapi.Util
+{
+    public class IdGenerator
+    {
+        private static int _maxsequence = 9999;
+        private static int _increment = 409;
+
+        private Random _random = new Random();
+        private int sequence = 0;
+
+        public string GetMonotonicId(string prefix)
+        {
+            sequence = (sequence % (_maxsequence - _increment)) + _random.Next(0, _increment);
+
+            return string.Format(CultureInfo.InvariantCulture, "{0}-{1}-{2}",
+                    prefix,
+                    DateTime.Now.ToUniversalTime().ToString("yyyyMMddHHmmssffffK", CultureInfo.InvariantCulture),
+                    sequence.ToString("D4", CultureInfo.InvariantCulture)
+                );
+        }
+    }
+}
