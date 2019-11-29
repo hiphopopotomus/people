905d97858385bc9c9e999d2c8dd2250d8035f3b7
Espen Gronli <espen@efficio-progressio.com>
2019-11-29 14:46:01 +0100

    Add converters for FhirPerson

--- web-api/Entities/FhirPerson.cs
+++ web-api/Entities/FhirPerson.cs
@@ -16,46 +16,46 @@ namespace webapi.Entities
 
         public static implicit operator Person(FhirPerson person) => TypeDescriptor.GetConverter(typeof(FhirPerson)).ConvertTo(person, typeof(Person)) as Person;
         public static implicit operator FhirPerson(Person person) => TypeDescriptor.GetConverter(typeof(FhirPerson)).ConvertFrom(person) as FhirPerson;
-    }
-
-    internal class FhirPersonConverter : TypeConverter
-    {
-        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+     
+        private class FhirPersonConverter : TypeConverter
         {
-            return typeof(Person) == sourceType;
-        }
-
-        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
-        {
-            var person = value as Person;
-
-            return new FhirPerson 
-            { 
-                id = person.documentId.ToString(),
-                active = person.active,
-                gender = person.gender,
-                birthDate = person.birthDate,
-                name = person.name
-            };
-        }
-
-        public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType)
-        {
-            return typeof(Person) == destinationType;
-        }
-
-        public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
-        {
-            var fhirPerson = value as FhirPerson;
-
-            return new Person
+            public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
             {
-                documentId = (fhirPerson.id == null) ? default(Guid) : Guid.Parse(fhirPerson.id),
-                active = fhirPerson.active,
-                gender = fhirPerson.gender,
-                birthDate = fhirPerson.birthDate,
-                name = fhirPerson.name
-            };
+                return typeof(Person) == sourceType;
+            }
+
+            public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+            {
+                var person = value as Person;
+
+                return new FhirPerson 
+                { 
+                    id = person.documentId.ToString(),
+                    active = person.active,
+                    gender = person.gender,
+                    birthDate = person.birthDate,
+                    name = person.name
+                };
+            }
+
+            public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType)
+            {
+                return typeof(Person) == destinationType;
+            }
+
+            public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
+            {
+                var fhirPerson = value as FhirPerson;
+
+                return new Person
+                {
+                    documentId = (fhirPerson.id == null) ? default : Guid.Parse(fhirPerson.id),
+                    active = fhirPerson.active,
+                    gender = fhirPerson.gender,
+                    birthDate = fhirPerson.birthDate,
+                    name = fhirPerson.name
+                };
+            }
         }
     }
 }
